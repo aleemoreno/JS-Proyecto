6ff042aaf102d65784999b11c4a410cce308e660
Alejandro Moreno <alejandromoreno02@live.com.ar>
2024-07-15 23:23:28 -0300

    js-primera entrega

--- js/script.js
+++ js/script.js
@@ -1,121 +1,82 @@
 
 
-/* Bienvenid al Usuario, ingresar nombre */
+/*  Bienvenid al Usuario, ingresar nombre  */
 
-function Bienvenida() {
+ function ingresarNombreDeUsuario() {
     
-do {
-    let nombreDeUsuario = prompt ("Por favor, ingrese su nombre");
+    let nombreDelUsuario = null;
 
-    if (nombreDeUsuario === null) {
-        return ;
-    }else if (/^[a-zA-Z]+$/.test(nombreDeUsuario)) {
-        alert ("Bienvenido " + nombreDeUsuario);
-    } else  {
-        alert ("Caracter invalido.");
+    while ((nombreDelUsuario === null || nombreDelUsuario === '')) {
+       nombreDelUsuario = prompt ('Por favor, registre un nombre de Usuario')
+
+       if ((nombreDelUsuario === null || nombreDelUsuario === '')) {
+        alert ('Debe ingresar un nombre de Usuario para acceder a NECA STORE')
+       }
+    }
+     alert ('Bienvenido ' +nombreDelUsuario) 
+} 
+
+/* Verificar edad del usuario */
+
+ function verificacionDeEdad() {
+    let edadDelUsuario = null;
+  12312
+    while (edadDelUsuario === null || isNaN(edadDelUsuario)) {
+      edadDelUsuario = prompt('Por favor, ingrese su edad');
+      
+      if (edadDelUsuario === null || isNaN(edadDelUsuario)) {
+        alert('Debe ingresar su edad para poder acceder a NECA STORE');
+      } else {
+        edadDelUsuario = parseInt(edadDelUsuario);
+        if (edadDelUsuario >= 18) {
+          alert('Bienvenido a NECA STORE');
+          return true; 
+        } else if (edadDelUsuario <= 17) {
+          alert('Lo siento, pero este sitio web es solo para mayores de 18');
+          return false;
+        }
+      }
+    }
+  }
+
+  if (verificacionDeEdad()) {
+    ingresarNombreDeUsuario();
+
+    /* Arrays */
+
+    let total = 0 
+    let bandera = true
+    const productosFiguras = ['1 - Jack Skellington', '2 - Tortugas Ninja', '3 - Frankestein', '4 - Dracula', '5 - E.T', '6 - Gargolyes' ]
+    
+    const Precios = [39, 39, 50, 50, 39, 39]
+    
+    function compraLogica(precio) {
+        total += precio   
+    }
+
+
+function productosLogica(numero) {
+    if(numero > productosFiguras.length || numero < 1) {
+        alert('Hubo un error, por favor seleccione un numero de producto')
+    }else {
+        let indice = numero - 1
+        compraLogica(Precios[indice])
+        bandera = confirm ('Quiere seguir comprando?')
     }
 }
 
-while (/^[a-zA-Z]+$/.test(nombreDeUsuario));
-}
+while(bandera){
+     let menu = 'Escriba el numero de la Figura que quiere comprar:\n' + productosFiguras.join('\n')
+     let seleccion = prompt(menu)
 
-Bienvenida ()
-
-
-/* Agregar al carrito */
-
-const addToCartButtons = document.querySelectorAll (".addtocart-button")
-
-
-function agregarAlCarrito() {
-    let cantidad = prompt ("Ingrese la cantidad a agregar al Carrito");
-
-if (cantidad === null) {
-
-} else if  (!isNaN(cantidad) && parseInt(cantidad) > 0) {
-
-    alert ("Se agrego " + cantidad + " figura(s) al carrito");
-
- } else {
-    alert ("Por favor ingrese un numero.");
+     if (seleccion === null || seleccion === '') {
+        alert('Por favor, seleccione un numero de producto')
+     }else {
+        productosLogica(parseInt(seleccion))
     }
+}    
+
+alert ('El total es:'+ total)
+
 }
-
-
-/* Arrays */
-
-
-const productos = [
-    {
-        id: "figura-01",
-        nombrefigura: "figura-01",
-        altura: "9”",
-        precio: "$34.99",
-        imagen: "./Images/jack-productoo.png"
-    },
-    {
-        id: "figura-02",
-        nombrefigura: "figura-02",
-        altura: "7”",
-        precio: "$36.99",
-        imagen:".Images/last ronin.png"
-    },
-    {
-        id: "figura-03",
-        nombrefigura: "figura-03",
-        altura: "7”",
-        precio: "$59.99",
-        imagen: ".Images/frankesteinproducto.png"
-    },
-    {
-        id: "figura-04",
-        nombrefigura: "figura-04",
-        altura: "9”",
-        precio: "$34.99",
-        imagen: ".Images/dracula-producto.png"
-    },
-    {
-        id: "figura-05",
-        nombrefigura: "figura-05",
-        altura: "9”",
-        precio: "$34.99",
-        imagen: ".Images/E.T-producto.png"
-    },
-    {
-        id: "figura-06",
-        nombrefigura: "figura-06",
-        altura: "9”",
-        precio: "$34.99",
-        imagen: ".Images/gargolyes-producto.png"
-    }
-];
-
-const contenedorProductos = document.querySelector("#contenedor-productosenstock");
-
-
-
-function cargarProductos() {
-
-    productos.forEach(producto => {
-
-       const div = document.createElement ("div");
-       div.classList.add ("producto");
-       div.innerHTML = `
-       <div class="producto">
-       <img src="${producto.imagen} alt="Figura de coleccion">
-       <div class="producto-detalles">
-         <h3 class="producto-titulo">${producto.nombrefigura}</h3>
-         <p class="producto-precio">${producto.precio}</p>
-         <button class="addtocart-button" id="${producto.id}"<p class="button-text">ADD TO CART</p></button>
-        </div>      
-       `;
-
-       contenedorProductos.append(div);
-    });
-}
-
-document.querySelectorAll(".addtocart-button").forEach(button => {
-    button.addEventListener("click", () => {
-       
-    })
-})
+  
